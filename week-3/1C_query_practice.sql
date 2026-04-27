ca156016081f0aebc5a544b1458b77d0c08419c6
yanuris campusano <ycampusano@my.yearupunited.org>
2026-04-27 01:13:17 -0400

    push documents

---
+++ week-3/1C_query_practice.sql
@@ -0,0 +1,67 @@
+Use northwind;
+/*1. Write a query to list the product id, product name, and unit price of every product. This
+time, display them in ascending order by price.*/
+SELECT ProductID , ProductName, UnitPrice
+FROM products
+ORDER BY UnitPrice ASC;
+
+/*2. What are the products that we carry where we have at least 100 units on hand? Order
+them in descending order by price.*/
+select ProductID, ProductName, UnitPrice, UnitsInStock 
+from products
+where UnitsInStock >= 100
+order by UnitPrice desc;
+
+
+/*3. What are the products that we carry where we have at least 100 units on hand? Order
+them in descending order by price. If two or more have the same price, list those in
+ascending order by product name.*/
+select ProductID, ProductName, UnitPrice, UnitsInStock
+from products
+where UnitsInStock >= 100
+order by UnitPrice desc, ProductName asc;
+
+
+/*4. Write a query against the orders table that displays the total number of distinct
+customers who have placed orders, based on customer ID. Use an alias to label the
+count calculation as CustomerCount.*/
+select count(distinct CustomerID) as CustomerCount 
+from orders;
+
+
+/*5. Write a query against the orders table that displays the total number of distinct
+customers who have placed orders, by customer ID, for each country where orders
+have been shipped. Again, use an alias to label the count as CustomerCount. Order
+the list by the CustomerCount, largest to smallest.*/
+select ShipCountry, count(distinct CustomerID) as CustomerCount
+from orders
+group by ShipCountry
+order by CustomerCount desc;
+
+
+/*6. What are the products that we carry where we have less than 25 units on hand, but 1
+or more units of them are on order? Write a query that orders them by quantity on
+order (high to low), then by product name.*/
+select ProductID, ProductName, UnitsInStock, UnitsOnOrder
+from products 
+where UnitsInStock < 25
+and UnitsOnOrder >= 1
+order by UnitsOnOrder desc, ProductName asc;
+
+
+/*7. Write a query to list each of the job titles in employees, along with a count of how
+many employees hold each job title.
+Year Up United Data Analyst Training Academy Week 3 Lab Workbook
+Page 8 of 16*/
+select title, count(*) as employee_count
+from employees
+group by title;
+
+
+/*8. What employees have a monthly salary that is between $2000 and $2500? Write a
+query that orders them by job title.*/
+select FirstName, LastName, title, (salary / 12) as monthly_salary
+from employees 
+where (salary/ 12) between 2000 and 2500
+order by title asc;
+
