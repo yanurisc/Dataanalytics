64df3825b97b2035f9f470e380f1aa25377038af
Yanuris Campusano <cyanuris@gmail.com>
2026-05-07 01:07:19 -0400

    Ex1A complete

--- week-5/Ex1A_hello.py
+++ week-5/Ex1A_hello.py
@@ -1,1 +1,19 @@
 print ('Hello world!')
+
+message = 'Hello world!'
+print (message)
+
+# "Hello world" prints twice because one uses a direct string and one uses a variable.
+
+dollars = 3
+cents = .50
+print (dollars + cents)
+
+# The dollars and cents were added together and the result is 3.5
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
