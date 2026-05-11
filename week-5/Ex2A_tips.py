cffba1c1473bb8bb85a3e0f9f3b293d8f058b7da
Yanuris Campusano <cyanuris@gmail.com>
2026-05-11 01:28:39 -0400

    push documents

---
+++ week-5/Ex2A_tips.py
@@ -0,0 +1,15 @@
+#Lab 1
+#Total due is determined by: food cost + tax + tip
+
+
+#Define known values
+food_cost = 79.25
+tax = 6.54
+trip = 12.00
+
+#calculate the unknown
+total_due = food_cost + tax + tip 
+
+#Display the results 
+print("the total due is " + str(total_due))
+
