de20ab31aac3ce7103012700d9a0974110ee4864
Yanuris Campusano <cyanuris@gmail.com>
2026-05-10 23:53:26 -0400

    Ex1B_variable_examples.py

---
+++ week-5/Ex1B_variable_examples.py
@@ -0,0 +1,38 @@
+# Lab 1 - 
+
+customer_id = 1002
+# using a number but might need to be a string if IDs start with zeros
+
+customer_name = "Maria Lopez"
+# stored as full name but separating first and last might be more useful
+
+customer_gender = "Female"
+# stored as "M" or "F" to save space
+
+customer_dob = "1982-09-23"
+# used a string in YYYY-MM-DD format for date of birth 
+
+drivers_license = "L874602836"
+# must be a string since license numbers contain both letters and numbers
+
+auto_policy_number = "Auto-11223"
+#  a string because it contains dashes
+
+
+#Create variables for: your own name, the city and state where you were born or grew up
+My_name = "Yanuris Campusano"
+birth_city_state = "New York, New York"
+
+#Lab 2 - Reserved words in Python that cannot be used as variable names:
+# False, def, if, raise, None, del, import, return True, elif, in, try, and, else, is, while, as, except, lambda,
+# with, assert, finally, nonlocal, yield, break, for, not, class, form, or, continue, global, pass
+
+# 5 keyword definitions:
+#if = check a condition and run code when that conditio is true
+#while = repeats code as long as a condition stays true
+#def = creates a reasuble block of code called a function
+#return = used to send a result back when fucntion finishes running 
+#import = add extra tools, its used to bring in external ^^modules^^ or libraries 
+
+    
+
